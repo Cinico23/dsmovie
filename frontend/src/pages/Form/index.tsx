6744c5d529b782644f218cc25aa10516d2cc36af
Rafael Leal <rafadigitus@gmail.com>
2022-05-11 09:12:21 -0300

    Security config

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -36,9 +36,9 @@ function Form() {
                     </div>
                 </form >
                 <Link to="/">
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-                    </Link>
-                
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
     );
